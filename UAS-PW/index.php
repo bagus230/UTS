48d096125309b2eb03bfef40a2f5a09afd7ef684
bagus230 <164632607+bagus230@users.noreply.github.com>
2024-06-26 20:58:35 +0800

    Add files via upload

---
+++ UAS-PW/index.php
@@ -0,0 +1,75 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Game Store </title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    <header>
+        <div class="container">
+            <div class="logo">
+                <a href="#">Game Store </a>
+        </div>
+        <nav class="navbar">
+            <ul>
+                <li><a href="#">Home</li>
+                <li><a href="#">Store</li>
+                <li><a href="#">Library</li>
+                <li><a href="#">Community</li>
+            </ul>
+        </nav>
+            <div class="user">
+                 <a href="#">Login</a>
+            </div>
+        </div>
+    </header>
+    <main>
+        <section class="hero">
+            <div class="container">
+                <h1>Welcome</h1>
+                <p>Discover and Play Amazing Game</p>
+            </div>        
+        </section class="game">
+            <div class="cotainer">
+                <h2>Rekomendasi New Game</h2>
+                <div class="game-list">
+                    <div class="jenis-game">
+                        <img scr="" alt="Game 1">
+                        <h3>PUBG</h3>
+                        <p>game seru dan sangat menyenangkan</p>
+                    </div>
+                </div>
+            </div> 
+            <div class="game-list">
+                    <div class="jenis-game">
+                        <img scr="" alt="Game 1">
+                        <h3>PUBG</h3>
+                        <p>game seru dan sangat menyenangkan</p>
+                    </div>
+                </div>
+            </div> 
+            <div class="game-list">
+                    <div class="jenis-game">
+                        <img scr="" alt="Game 1">
+                        <h3>PUBG</h3>
+                        <p>game seru dan sangat menyenangkan</p>
+                    </div>
+                </div>
+            </div> 
+        <section>
+        </section>
+        
+    </main>
+    <footer>
+        <div class="container">
+            <p><&copy; 2024 MyStore. All Rights Reserved.</p>
+        </div>
+    </footer>
+</body>
+</html>
